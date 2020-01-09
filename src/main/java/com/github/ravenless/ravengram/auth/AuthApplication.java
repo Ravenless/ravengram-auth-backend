ce8a2c198088aff037f4b8ba82f3d1ff76069448
Aleksey Voronenkov <aleksey@4u.house>
2020-01-09 18:54:51 +0300

    Initial commit

---
+++ src/main/java/com/github/ravenless/ravengram/auth/AuthApplication.java
@@ -0,0 +1,19 @@
+package com.github.ravenless.ravengram.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main
+ *
+ * @author Voronenkov Aleksei
+ * @since 09.01.2020
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
